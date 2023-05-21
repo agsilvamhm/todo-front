0e34cb2cbf0ea112f745acb8d8966aaef46fd7e9
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2023-05-21 18:11:52 -0300

    Commit final do projeto

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Todo } from 'src/app/models/todo';
 import { TodoService } from 'src/app/services/todo.service';
 
@@ -17,9 +17,27 @@ export class UpdateComponent implements OnInit {
     finalizado: false
   }
 
-  constructor(private router: Router, private service: TodoService) { }
+  constructor(private router: Router, private service: TodoService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.todo.id = this.route.snapshot.paramMap.get("id")!;
+    this.findById();
+  }
+
+  findById():void{
+    this.service.findById(this.todo.id).subscribe((resposta) => {
+      this.todo = resposta;
+    })
+  }
+
+  update(): void{
+    this.service.update(this.todo).subscribe((resposta)=> {
+      this.service.message('Informações atualizadas com sucesso!');
+      this.router.navigate([''])
+    }, error => {
+      this.service.message('Falha ao atualizar To-do');
+      this.router.navigate(['']);
+    });
   }
 
   cancel(): void {
@@ -30,5 +48,4 @@ export class UpdateComponent implements OnInit {
     let data = new Date(this.todo.dataParaFinalizar);
     this.todo.dataParaFinalizar = `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
   }
-
-}
+} 
